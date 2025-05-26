612013112497b6ad1ae921a526123df3e9f285f2
fbarreto05 <fernandobarreto864@gmail.com>
2025-05-26 20:13:23 -0300

    Tentativa de manter o ambiente de desenvolvimento

--- TaEmCasa/settings.py
+++ TaEmCasa/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@u*!*vh(6i-7^s-ve5xtx3yiuuj*lx$l_148alwggzx1enr&76'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['3.137.41.200']
 
@@ -118,8 +118,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = os.path.join('/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

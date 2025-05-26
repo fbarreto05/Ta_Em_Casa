21ade029f1850fb4c57fb60ef68d67b2b97b5bf7
fbarreto05 <fernandobarreto864@gmail.com>
2025-05-26 19:18:53 -0300

    Adição do host

--- TaEmCasa/settings.py
+++ TaEmCasa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@u*!*vh(6i-7^s-ve5xtx3yiuuj*lx$l_148alwggzx1enr&76
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.137.41.200']
 
 
 # Application definition

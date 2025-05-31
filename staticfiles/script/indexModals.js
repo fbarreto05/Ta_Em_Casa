25b076ffa2d0d9839a881c9b3cf25f64fd918184
fbarreto05 <fernandobarreto864@gmail.com>
2025-05-31 19:04:36 +0000

    Finalização do sistema

---
+++ staticfiles/script/indexModals.js
@@ -0,0 +1,41 @@
+function openModal(id, negocioId = null) {
+  const modal = document.getElementById(id);
+  modal.style.display = 'flex';
+
+  if (negocioId != null) {
+    const checkboxes = document.getElementsByName('tipo');
+    const tiposSelecionados = [];
+
+    checkboxes.forEach(chk => {
+      if (chk.checked) {
+        tiposSelecionados.push(chk.value);
+      }
+    });
+
+    const raioElem = document.getElementsByName('raio')[0];
+    const raioSelecionado = raioElem ? raioElem.value : '';
+
+    let query = `?raio=${encodeURIComponent(raioSelecionado)}`;
+    tiposSelecionados.forEach(tipo => {
+      query += `&tipo=${encodeURIComponent(tipo)}`;
+    });
+
+    const form = modal.querySelector("form");
+    form.action = `/login_validate/?query=${encodeURIComponent(query)}&negocio=${negocioId}`;
+  }
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+window.onclick = function(event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+}
+
+
